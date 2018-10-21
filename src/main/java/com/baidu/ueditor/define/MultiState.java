241fc91b1c1a12fa7dc1bb00693a8a0450e1215e
lihy <529568252@qq.com>
2018-10-21 11:44:39 +0800

    图片列表json错误

--- src/main/java/com/baidu/ueditor/define/MultiState.java
+++ src/main/java/com/baidu/ueditor/define/MultiState.java
@@ -55,53 +55,30 @@ public class MultiState implements State {
 
 	@Override
 	public String toJSONString() {
-		
 		String stateVal = this.isSuccess() ? AppInfo.getStateInfo( AppInfo.SUCCESS ) : this.info;
-		
 		StringBuilder builder = new StringBuilder();
-		
 		builder.append( "{\"state\": \"" + stateVal + "\"" );
-		
 		// 数字转换
 		Iterator<String> iterator = this.intMap.keySet().iterator();
-		
 		while ( iterator.hasNext() ) {
-			
 			stateVal = iterator.next();
-			
 			builder.append( ",\""+ stateVal +"\": " + this.intMap.get( stateVal ) );
-			
 		}
-		
 		iterator = this.infoMap.keySet().iterator();
-		
 		while ( iterator.hasNext() ) {
-			
 			stateVal = iterator.next();
-			
 			builder.append( ",\""+ stateVal +"\": \"" + this.infoMap.get( stateVal ) + "\"" );
-			
 		}
-
-		builder.append( ",\""+"list"+"\":[" );
-		
-		
+		builder.append(",\"list\":[");
 		iterator = this.stateList.iterator();
-		
 		while ( iterator.hasNext() ) {
-			
 			builder.append( iterator.next() + "," );
-			
 		}
-		
 		if ( this.stateList.size() > 0 ) {
 			builder.deleteCharAt( builder.length() - 1 );
 		}
-		
 		builder.append( " ]}" );
-
 		return Encoder.toUnicode( builder.toString() );
-
 	}
 
 	@Override
